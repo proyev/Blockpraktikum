9d757752e4ad2e7cb19accd19529b8785ea3f075
Yevhen <prots.yevhen@gmail.com>
2021-03-03 22:13:29 +0100

    Task 2 - basic set-up

---
+++ course/week_1/day_3/3_2/linkedlist.h
@@ -0,0 +1,16 @@
+#ifndef INC_3_2_LINKEDLIST_H
+#define INC_3_2_LINKEDLIST_H
+#include <iostream>
+
+class LinkedList{
+    //class ListElement();
+};
+
+class  ListElement{
+
+};
+
+template<class T> void printList(const LinkedList<T> &list);
+
+
+#endif //INC_3_2_LINKEDLIST_H
---
+++ course/week_1/day_3/3_2/test_linkedlist.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "linkedlist.h"
+
+int main() {
+    int length;
+    std::cout << "Enter the linked list length: " << std::flush;
+    std::cin >> length;
+
+//    for (int i = 0; i < length; i++){
+//        elem = i * 5;
+//    }
+
+    return 0;
+}
