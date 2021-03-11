25cd41ec0d8426ffe335498a4c7c9ba37735d309
Yevhen <prots.yevhen@gmail.com>
2021-03-11 15:23:17 +0100

    argc argv - stuff

---
+++ course/exam_prep/d1/vl1_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char **argv){
+	
+	std::cout << "Exam prep, bitch" << std::endl;
+	std::cout << argv[0] << std::endl;
+	std::cout << argc << std::endl;
+	std::cout << argv[1] << std::endl;
+	std::cout << argv[2] << std::endl;
+	return 0;
+}
