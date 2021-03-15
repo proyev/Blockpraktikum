9fded6516580db892c55d339f28d5b52150847a4
Yevhen <prots.yevhen@gmail.com>
2021-03-15 20:31:47 +0100

    First week hws

---
+++ course/week_1/day_2/2_3/build/CMakeFiles/car.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/car.dir/car.cpp.o"
+  "CMakeFiles/car.dir/cartest.cpp.o"
+  "car"
+  "car.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/car.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
