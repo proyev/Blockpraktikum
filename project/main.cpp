2c19884ce0165e80a83465bda964fde1b7216f16
Yevhen <prots.yevhen@gmail.com>
2021-03-22 20:18:52 +0100

    Updated features

--- project/main.cpp
+++ project/main.cpp
@@ -12,14 +12,21 @@ int main(int argc, char *argv[])
 
     Logic *logic = new Logic;
 
-    QWidget::connect(&w, &battleships::positionPlayer,
-                     logic, &Logic::testSlot);
     QWidget::connect(&w, &battleships::shipOrientation,
                      logic, &Logic::slotOrientation);
     QWidget::connect(&w, &battleships::createShip,
                      logic, &Logic::createShip);
     QWidget::connect(&w, &battleships::positionPlayer,
-                     logic, &Logic::getStartPosition);
+                     logic, &Logic::getStartPosition);//here the check-position should be called within logic
+    //if ok - emit drawShip to pass to gameSetup all the needed info - the ship will be drawn and the game will be updated
+    //if not ok - emit status with some marker:
+    // p - for problem with placement or similiar
+    // a - awaiting another player
+    // c - connection establilshed
+    // t - your turn
+    // o - your opponents turn
+    // w - you win
+    // l - game over - you lost
 
 //    QWidget::connect(&w, &battleships::positionRival,
 //                     logic, &Logic::getEndPosition);
@@ -27,7 +34,7 @@ int main(int argc, char *argv[])
 //    QWidget::connect(&logic,
 
     QWidget::connect(logic, &Logic::draw,
-                     &w, &battleships::drawShip);
+                     &w, &battleships::gameSetup);
 
 
     return a.exec();
